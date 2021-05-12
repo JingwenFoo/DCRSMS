b6ddb7e9bc1ad9285d04403ee4cb7eee48b59c9e
JingwenFoo <jwen98.jwf@gmail.com>
2021-05-12 11:27:16 +0800

    Update serviceRequestController.php

--- Controller/serviceRequestController.php
+++ Controller/serviceRequestController.php
@@ -7,6 +7,20 @@ class serviceRequestController
 	function addSerReq()
 	{
 		$request = new serviceRequestModel();
-		$request->
+		
+		$request->custID = $_POST['custID'];
+		$request->serviceType = $_POST['serviceType'];
+		$request->symptom = $_POST['symptom'];
+		$request->damageInfo = $_POST['damageInfo'];
+		$request->requestStatus = $_POST['requestStatus'];
+		$request->requestDate = $_POST['requestDate'];
+		$request->requestTime = $_POST['requestTime'];
+
+		if($request->addSerReq()>0)
+		{
+			 $message = "Add Service Request Success!";
+            echo "<script type='text/javascript'>alert('$message');
+            window.location = '../../ManageServicesRequest/addServiceRequest.php';</script>";
+		}
 	}
 }
