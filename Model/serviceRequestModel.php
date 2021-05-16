2014a1c3eacac10ed1b7aab63deb9958c150d456
JingwenFoo <jwen98.jwf@gmail.com>
2021-05-17 01:08:00 +0800

    Update serviceRequestModel.php

--- Model/serviceRequestModel.php
+++ Model/serviceRequestModel.php
@@ -1,62 +1,84 @@
 <?php
-class DB
-{
-
-  public static function connect($value='')
-  {
-    // define a constant variable to store our database settings
-    define('DB_TYPE', 'mysql');
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'dcrsms');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    // create a new PDO connection
-    $pdo = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    return $pdo;
-  }
-
-  public static function run($sql, $args = NULL)
-    {
-        if (!$args) // if no parameter
-        {
-          // run the query straight away without parameter binding
-             return DB::connect()->query($sql);
-        }
-        // prepare the sql query
-        $stmt = DB::connect()->prepare($sql);
-        // execute the query with bind parameter values
-        try {
-            $stmt->execute($args);
-            return $stmt;
-        }
-        catch (PDOException $e){
-            echo $e->getMessage();
-            die();
-        }   
-     }
-}
+require_once '../libs/database.php';
 
 class serviceRequestModel
 {
-	public $custID, $staffID, $repairStaffID, $serviceType, $symptom, $damageInfo, $requestStatus, $requestDate, $requestTime, $amountPrice, $requestDetail, $requestProgress;
-
-	function addSerReq()
-	{
-		$sql = "insert into serviceRequestQuotation (custID, staffID, repairStaffID, serviceType, symptom, damageInfo, requestStatus, requestDate, requestTime, amountPrice, requestDetail, requestProgress) values(:custID, :staffID, :repairStaffID, :serviceType, :symptom, :damageInfo, :requestStatus, :requestDate, :requestTime, :amountPrice, :requestDetail, :requestProgress)";
+    public $custID, $staffID, $repairStaffID, $serviceType, $symptom, $damageInfo, $requestStatus, $requestDate, $requestTime, $amountPrice, $requestDetail, $requestProgress;
+    
+    //add service request into database
+    function addSerRequest()
+    {
+        $sql = "insert into serviceRequestQuotation (custID, staffID, repairStaffID, serviceType, symptom, damageInfo, requestStatus, requestDate, requestTime, amountPrice, requestDetail, requestProgress) values(:custID, :staffID, :repairStaffID, :serviceType, :symptom, :damageInfo, :requestStatus, :requestDate, :requestTime, :amountPrice, :requestDetail, :requestProgress)";
         $args = [':custID'=>$this->custID, ':staffID'=>$this->staffID, ':repairStaffID'=>$this->repairStaffID, ':serviceType'=>$this->serviceType, ':symptom'=>$this->symptom, ':damageInfo'=>$this->damageInfo, ':requestStatus'=>$this->requestStatus, ':requestDate'=>$this->requestDate, ':requestTime'=>$this->requestTime, ':amountPrice'=>$this->amountPrice, ':requestDetail'=>$this->requestDetail, ':requestProgress'=>$this->requestProgress];
         $stmt = DB::run($sql, $args);
         $count = $stmt->rowCount();
         return $count;
+<<<<<<< Updated upstream
 	}
 
     function viewList()
+=======
+    }
+
+    //customer view service request list
+    function viewQuotationList()
+>>>>>>> Stashed changes
     {
         $sql = "Select * from serviceRequestQuotation where custID=:custID";
         $args = [':custID'=>$this->custID];
         return DB::run($sql,$args);
     }
+<<<<<<< Updated upstream
+=======
+
+    //customer view service request detail
+    function viewQuotationDetail()
+    {
+        $sql = "select * from serviceRequestQuotation inner join customer on serviceRequestQuotation.custID = customer.custID where serviceRequestQuotation.serReqID=:serReqID";
+        $args = [':serReqID'=>$this->serReqID];
+        return DB::run($sql,$args);
+    }
+
+    //customer delete request row
+    function deleteRequest()
+    {
+        $sql = "delete from serviceRequestQuotation where serReqID=:serReqID";
+        $args = [':serReqID'=>$this->serReqID];
+        return DB::run($sql,$args);
+    }
+
+    //customer update service request quotation
+    function editServiceRequest()
+    {
+        $sql = "update serviceRequestQuotation set serviceType=:serviceType, symptom=:symptom, damageInfo=:damageInfo where serReqID=:serReqID";
+        $args = [':serviceType'=>$this->serviceType, ':symptom'=>$this->symptom, ':damageInfo'=>$this->damageInfo, ':serReqID'=>$this->serReqID];
+        return DB::run($sql,$args);
+    }
+
+    //staff view all the submitted service request list
+    function viewList()
+    {
+         $sql = "select * from serviceRequestQuotation inner join customer on serviceRequestQuotation.custID = customer.custID where requestStatus=:requestStatus";
+        $args = [':requestStatus'=>$this->requestStatus];
+        return DB::run($sql,$args);
+    }
+
+    //staff view all the confirmed service request list
+    function searchRequest()
+    {
+        $sql = "select * from serviceRequestQuotation inner join customer on serviceRequestQuotation.custID = customer.custID where requestStatus=:requestStatus";
+        $args = [':requestStatus'=>$this->requestStatus];
+        return DB::run($sql,$args);
+    }
+
+    //staff confirm the submitted service request quotation
+   function confirmServiceRequest()
+   {
+        $sql = "update serviceRequestQuotation set staffID=:staffID, requestStatus=:requestStatus where serReqID=:serReqID";
+        $args = [':staffID'=>$this->staffID, ':requestStatus'=>$this->requestStatus, ':serReqID'=>$this->serReqID];
+        return DB::run($sql,$args);
+   }
+>>>>>>> Stashed changes
 }
 
 ?>
