8ab7b32bfd483695b57d45635bab40aa75d69ad6
JingwenFoo <jwen98.jwf@gmail.com>
2021-05-17 01:07:46 +0800

    Update viewList.php

--- ManageServicesRequest/viewList.php
+++ ManageServicesRequest/viewList.php
@@ -3,10 +3,32 @@ session_start();
 require_once '../Controller/serviceRequestController.php';
 include '../index.html';
 
+<<<<<<< Updated upstream
 
 $custID = $_POST['custID'];
 $request = new serviceRequestController();
 $data = $request->viewList();
+=======
+$request = new serviceRequestController();
+$custID = $_SESSION['cid'];
+$data = $request->viewQuotationList();
+
+//delete service request row
+if(isset($_POST['delete']))
+{
+  $request = new serviceRequestController();
+  $serReqID = $_POST['deleteID'];
+  $data = $request->deleteRequest($serReqID);
+}
+
+//view detail of service request
+if(isset($_POST['view']))
+{
+  $request = new serviceRequestController();
+  $serReqID = $_POST['serReqID'];
+  $data = $request->viewQuotationDetail($serReqID);
+}
+>>>>>>> Stashed changes
 ?>
 <html>
 <head>
@@ -21,7 +43,12 @@ $data = $request->viewList();
 <body>
 <div style='margin-left:300px;padding:70px;height:1000px;'>
  <h1>Service Request List</h1><hr/><br/>
+<<<<<<< Updated upstream
  <table id="table" class="table table-striped table-bordered table-responsive-md" width="100%">
+=======
+ <!--Table shows all the list of service request made by customer-->
+ <table id="table" class="table table-striped table-bordered table-responsive-md">
+>>>>>>> Stashed changes
         <thead>
     <tr >
       <th style="text-align: center;">NO.</th>
@@ -39,15 +66,27 @@ $data = $request->viewList();
     <td style="text-align: center;"><?php echo $i+1?></td>
     <td style="text-align: center;"><?= $row['serReqID']?></td>
     <td style="text-align: center;"><?= $row['requestDate']?></td>
+<<<<<<< Updated upstream
     <td style="text-align: center;"><form action="" method="POST">
       <button style="background-color:#008CBA"; name="view"><i class="fa fa-eye"></i> View </button>&emsp;&emsp;&emsp;
        <button style="background-color:#4CAF50" name="edit"><i class="fa fa-edit"></i> Edit </button>&emsp;&emsp;&emsp;
        <button style="background-color: #f44336;" name="delete"><i class="fa fa-trash"></i> Delete </button>
+=======
+    <td style="text-align: center;"><form action="viewForm.php?id=<?=$row['serReqID']?>" method="POST">
+      <button style="background-color:#008CBA"; name="view"><i class="fa fa-eye"></i> View </button>&emsp;&emsp;&emsp;</form>
+      <form action="editForm.php?id=<?=$row['serReqID']?>" method="POST">
+       <button style="background-color:#4CAF50" name="edit"><i class="fa fa-edit"></i> Edit </button>&emsp;&emsp;&emsp;</form>
+       <form action="" method="POST">
+        <input type="hidden" name="deleteID" value="<?=$row['serReqID']?>">
+       <button style="background-color: #f44336;" name="delete" onclick="return confirm('Are you sure to delete?');"><i class="fa fa-trash"></i> Delete </button>
+>>>>>>> Stashed changes
     </form>
     </td>
   </tr>
+  
   <?php $i++; } ?>
 </tbody>
+<input type="text" name="custID" value="<?php echo $custID?>">
 <script type="text/javascript">
         $(document).ready(function(){
             $('table').DataTable();
